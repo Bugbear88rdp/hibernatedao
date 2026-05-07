3131a33d7c365d1184c738a9773b481910c3e4ae
Bugbear88rdp <ershov.k94@gmail.com>
2026-05-07 22:09:05 +0300

    initial commit

---
+++ src/main/java/com/example/hibernatedao/repository/PersonRepository.java
@@ -0,0 +1,20 @@
+package com.example.hibernatedao.repository;
+
+import com.example.hibernatedao.entity.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Person> getPersonsByCity(String city) {
+        return entityManager.createQuery("SELECT p FROM Person p WHERE p.cityOfLiving = :city", Person.class)
+                .setParameter("city", city)
+                .getResultList();
+    }
+}
---
+++ src/test/java/com/example/hibernatedao/HibernatedaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernatedao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatedaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
