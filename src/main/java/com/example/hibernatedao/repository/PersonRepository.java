82a40a3fb9125cf2e03aebc3b72f4ca3b0752f72
Bugbear88rdp <ershov.k94@gmail.com>
2026-05-10 12:09:48 +0300

    refactor PersoRepository

--- src/main/java/com/example/hibernatedao/repository/PersonRepository.java
+++ src/main/java/com/example/hibernatedao/repository/PersonRepository.java
@@ -3,13 +3,22 @@ package com.example.hibernatedao.repository;
 import com.example.hibernatedao.entity.Person;
 import com.example.hibernatedao.entity.PersonId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface PersonRepository extends JpaRepository<Person, PersonId> {
-    List<Person> findByCityOfLiving(String city);
-    List<Person> findByAgeLessThanOrderByAgeAsc(Integer age);
-    Optional<Person> findByNameAndSurname(String name, String surname);
+    @Query(value = "SELECT * FROM persons WHERE city_of_living = :city", nativeQuery = true)
+    List<Person> findByCityOfLiving(@Param("city") String city);
+
+    @Query(value = "SELECT * FROM persons WHERE age < :age ORDER BY age ASC", nativeQuery = true)
+    List<Person> findByAgeLessThanOrderByAgeAsc(@Param("age") Integer age);
+
+    @Query(value = "SELECT * FROM persons WHERE name = :name AND surname = :surname", nativeQuery = true)
+    Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
+
+
